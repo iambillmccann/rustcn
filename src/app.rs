bd54afdf3a7f1490c0aa2578f82e7dc28cf0dfbc
Bill McCann <bill.mccann@gmail.com>
2024-06-18 14:36:52 -0400

    Saving before making corrections

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,5 @@
 use dioxus::prelude::*;
 use dioxus_router::{Route, Router};
-mod about;
-mod home;
 
 #[component]
 pub fn App() -> Element {
@@ -9,6 +7,42 @@ pub fn App() -> Element {
         Router {
             Route { to: "/", component: home::Home },
             Route { to: "/about", component: about::About },
+        }
+    }
+}
+
+#[component]
+pub fn Home() -> Element {
+    rsx! {
+        div {
+            class: "p-4",
+            "Home Page",
+            Button {
+                text: "Click Me",
+                variant: Some(ButtonVariant::Default),
+                size: Some(ButtonSize::Default),
+                on_click: move |_| {
+                    println!("Button clicked!");
+                }
+            },
+            Button {
+                text: "Delete",
+                variant: Some(ButtonVariant::Destructive),
+                size: Some(ButtonSize::Sm),
+                on_click: move |_| {
+                    println!("Destructive button clicked!");
+                }
+            }
+        }
+    }
+}
+
+#[component]
+pub fn About() -> Element {
+    render! {
+        div {
+            class: "p-4",
+            "About Page"
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use dioxus::prelude::*;
+// use dioxus_web::launch;
 mod app;
 
 fn main() {
-    dioxus::web::launch(app::App);
+    launch(app::App);
 }
